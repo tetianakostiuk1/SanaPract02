2c97565910853904f73a4ae416a8b239fc3ecc48
tetianakostiuk <ipz213_kto@student.ztu.edu.ua>
2022-11-13 14:03:57 +0200

    last 2 rows changed

--- SanaPract02/Task1/Program.cs
+++ SanaPract02/Task1/Program.cs
@@ -3,6 +3,6 @@
 Console.WriteLine("111 ");
 Console.WriteLine("222");
 Console.WriteLine("333 ");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp ");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp ");
+Console.WriteLine("444 ");
+Console.WriteLine("555 ");
 
